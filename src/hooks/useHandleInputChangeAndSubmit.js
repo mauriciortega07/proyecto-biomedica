6048692b8aa008e8bfc4c5211ea79387fd303796
mauriciortega07 <mauriciortega93@gmail.com>
2025-06-29 05:53:02 -0600

    Se ajusto el fetch

--- src/hooks/useHandleInputChangeAndSubmit.js
+++ src/hooks/useHandleInputChangeAndSubmit.js
@@ -27,7 +27,7 @@ const useHandleInputChangeAndSubmit = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch("http://localhost:4000/register", {
+            const response = await fetch("https://backend-proyecto-biomedica-production.up.railway.app/register", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 credentials: "include",
@@ -56,7 +56,7 @@ const useHandleInputChangeAndSubmit = () => {
         e.preventDefault();
 
         try {
-            const response = await fetch("http://localhost:4000/login", {
+            const response = await fetch("https://backend-proyecto-biomedica-production.up.railway.app/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 credentials: "include",
