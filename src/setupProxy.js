5c625a660735081ce8f2a34222a508198eb8707e
mauriciortega07 <mauriciortega93@gmail.com>
2026-02-21 17:32:50 -0600

    Se cambia ip de instancia

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://3.87.154.111:4000',   // o http://localhost:4000 si levantarás backend local
+      target: 'http://ec2-3-87-154-111.compute-1.amazonaws.com:4000',   // o http://localhost:4000 si levantarás backend local
       changeOrigin: true,
       pathRewrite: { '^/api': '' },
     })
