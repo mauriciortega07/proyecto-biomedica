4ff1b3aede81cfcb8a7ac813edfd3c6c7442d4a0
mauriciortega07 <mauriciortega93@gmail.com>
2026-02-22 05:22:48 -0600

    ajustes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://172.31.21.247:4000',   // o http://localhost:4000 si levantarás backend local
+      target: 'http://172.31.21.247;4000',   // o http://localhost:4000 si levantarás backend local
       changeOrigin: true,
       pathRewrite: { '^/api': '' },
     })
